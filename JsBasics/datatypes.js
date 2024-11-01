e69e4abe5260808ee28df376d44894507c3c77f0
Ayesha Farzand <145141050+AyeshaFarzand@users.noreply.github.com>
2024-11-01 15:37:27 +0500

    datatypes, typeof

---
+++ JsBasics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict" //treat all js code as newer version
+
+alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +3);  //code readibility should be high
+
+    console.log("Ayesha");
+
+
+    let name="Ayesha"
+    let age= 21
+    let loggedIn= false
+
+
+    //number => 2 power of 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined 
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof null);            //output object
+    console.log(typeof undefined); //output undefined
+
+    
+    
+    
+
