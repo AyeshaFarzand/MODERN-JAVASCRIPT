66b65e4384b839cdbcb3be558e825cd81871851a
Ayesha Farzand <145141050+AyeshaFarzand@users.noreply.github.com>
2024-11-01 17:28:53 +0500

    conversion

---
+++ JsBasics/operations_conversion.js
@@ -0,0 +1,29 @@
+let str1="ayesha"
+let str2=" farzand"
+let str3=str1+str2
+console.log(str3);
+
+
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log((3 + 4) * 5 % 4);
+
+
+
+console.log(true);
+console.log(+true);
+//console.log(true+);
+console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3 = 2+2
+
+let gamecounter=100
+gamecounter++;                   //++gamecounter; //both output 101
+console.log(gamecounter);
+
+
+
