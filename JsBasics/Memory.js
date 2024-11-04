07b73b9fa0582de467d9b2ea12ba1735158feeca
Ayesha Farzand <145141050+AyeshaFarzand@users.noreply.github.com>
2024-11-04 14:07:53 +0500

    stack/Heap

---
+++ JsBasics/Memory.js
@@ -0,0 +1,20 @@
+// Heap (Primitive)           // stack (Non-Primitive)
+let myname="Maroosh"
+let anothername = myname
+anothername="Ayesha"
+
+console.log(myname);
+console.log(anothername);
+
+let userone = {
+    email:"ayesha@gmail.com",
+    id:1234
+}
+let usertwo=userone;
+usertwo.id=5678
+
+console.log(userone.id);
+console.log(usertwo.id);
+
+
+
